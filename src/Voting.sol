5ea123b83d3d5b19c3f8ee6e93d666f9e5c02191
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2025-03-18 15:32:45 +0530

    Voting smart Contract

--- src/Voting.sol
+++ src/Voting.sol
@@ -2,4 +2,129 @@
 
 pragma solidity ^0.8.19;
 
-contract Voting {}
+/**
+ * @title Simple Voting System Smart Contract
+ * @author Anmol Tuteja
+ * @notice This contract allows the creation of a basic voting system.
+ * @dev --ignore
+ */
+
+contract Voting {
+
+    error Voting__roomIdDoesNotExist();
+    error Voting__candidateRegisterationTimeOut();
+    error Voting__votingHasNotStarted();
+    error Voting__votingHasEnded();
+    error Voting__CanOnlyVoteOnce();
+    error Voting__votingHasNotEnded();
+    error Voting__CandidateAlreadyRegistered();
+
+     event WinnerPicked(uint256 indexed roomId, address[] indexed winner);
+
+    uint256 s_roomCount = 0;
+    mapping(uint256 => Room) public s_rooms;
+
+    struct Room {
+        uint256 id;
+        address creator;
+        address[] voters;
+        address[] candidates;
+        address[] winners;
+        uint256 candidateRegisterationEndTime;
+        uint256 votingStartTime;
+        uint256 votingEndTime;
+
+        mapping(address => bool) hasVoted;
+        mapping(address => uint256) voteCount;
+    }
+
+    struct Voter {
+        address voter;
+    }
+
+    struct Candidate {
+        address candidate;
+        uint256[] roomsParticipated;
+    }
+
+    function createRoom(uint256 candidateRegisterationWindow,uint256 votingStartTime,uint256 votingEndTime) public returns (uint256) {
+        s_roomCount++;
+        uint256 newRoomId = s_roomCount;
+
+        Room storage newRoom = s_rooms[newRoomId];
+        newRoom.id = newRoomId;
+        newRoom.creator = msg.sender;
+        newRoom.candidateRegisterationEndTime = block.timestamp + candidateRegisterationWindow;
+        newRoom.votingStartTime = votingStartTime;
+        newRoom.votingEndTime = votingEndTime;
+
+        return s_roomCount;
+    }
+
+    function registerCandidate(uint256 roomId) public {
+        if(s_roomCount<roomId){
+            revert Voting__roomIdDoesNotExist();
+        }
+    
+        if(block.timestamp > s_rooms[roomId].candidateRegisterationEndTime){
+            revert  Voting__candidateRegisterationTimeOut();
+        }
+
+        for (uint256 i = 0; i < s_rooms[roomId].candidates.length; i++) {
+            if (s_rooms[roomId].candidates[i] == msg.sender){
+                revert Voting__CandidateAlreadyRegistered();
+            }
+        }
+
+        Room storage room = s_rooms[roomId];
+        room.candidates.push(msg.sender);
+        room.voteCount[msg.sender] = 0;
+    }
+
+    function voteForCandidate(uint256 roomId,address candidate) public {
+       if(s_roomCount<roomId){
+            revert Voting__roomIdDoesNotExist();
+        }
+        if(block.timestamp < s_rooms[roomId].votingStartTime){
+            revert Voting__votingHasNotStarted();
+        }
+        if(block.timestamp > s_rooms[roomId].votingEndTime){
+            revert Voting__votingHasEnded();
+        }
+        if(s_rooms[roomId].hasVoted[msg.sender]){
+            revert Voting__CanOnlyVoteOnce();
+        }
+
+        Room storage room = s_rooms[roomId];
+        room.voteCount[candidate]+=1;
+        room.hasVoted[msg.sender] = true;
+    }
+
+
+    function max(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a > b ? a : b;
+    }
+
+    function declareResult(uint256 roomId) public {
+        if(s_roomCount<roomId){
+            revert Voting__roomIdDoesNotExist();
+        }
+
+        if(block.timestamp <= s_rooms[roomId].votingEndTime){
+            revert Voting__votingHasNotEnded();
+        }
+
+        uint256 length = s_rooms[roomId].candidates.length;
+        uint256 maxVotes;
+        Room storage room = s_rooms[roomId];
+
+        for(uint256 i=0;i<length;i++){
+            maxVotes = max(maxVotes,s_rooms[roomId].voteCount[room.candidates[i]]);
+        }
+
+        for(uint256 i=0;i<length;i++){
+            if(s_rooms[roomId].voteCount[room.candidates[i]] == maxVotes) room.winners.push(room.candidates[i]);
+        }
+        emit WinnerPicked(roomId,room.winners);
+    }
+}
---
+++ test/Voting.t.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {Test} from "lib/forge-std/src/Test.sol";
+
+contract VotingTest is Test {}
